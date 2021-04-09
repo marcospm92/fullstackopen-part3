f6945e3f0a78b9692ce7c3080c28b1c059ad1216
Marcos Perez Martin <marcospm92@gmail.com>
2021-04-10 01:13:16 +0200

    Exercise 3.9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -9,6 +10,7 @@ morgan.token('data', function getData (req) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(express.json())
+app.use(cors())
 
 let persons = [
   {
